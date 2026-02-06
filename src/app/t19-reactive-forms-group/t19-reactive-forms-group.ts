fa6a9a2b2cd0259669b15b56ed33107f82eb5528
supekar-aniket <supekaraniket34@gmail.com>
2026-02-06 12:25:40 +0530

    Add reactive validations in reactive form.

--- src/app/t19-reactive-forms-group/t19-reactive-forms-group.ts
+++ src/app/t19-reactive-forms-group/t19-reactive-forms-group.ts
@@ -1,18 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-t19-reactive-forms-group',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './t19-reactive-forms-group.html',
   styleUrl: './t19-reactive-forms-group.css',
 })
 export class T19ReactiveFormsGroup {
 
   profileForm = new FormGroup({
-    name : new FormControl(),
-    email : new FormControl(),
-    password : new FormControl()
+    name : new FormControl('',[Validators.maxLength(50),Validators.required]),
+    email : new FormControl('',[Validators.email,Validators.required]),
+    password : new FormControl('',[Validators.minLength(6),Validators.required])
   })
 
   onSubmit(){
